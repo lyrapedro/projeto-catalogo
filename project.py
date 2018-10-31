c4573696f06d20fdcd03a8d01063c16ac1a4926e
Pedro Lyra <38837397+lyrapedro@users.noreply.github.com>
2018-10-31 11:41:47 -0300

    guia de estilo pep8

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, jsonify,
-url_for, flash
+from flask import Flask, render_template, request, redirect, jsonify
+from flask import url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem, User
